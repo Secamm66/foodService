a641f9cf50e73fffeccd3e33c175109d05e14617
Vladimir Ershov <secamm66@mail.ru>
2025-01-03 00:37:10 +0300

    Enable RabbitMQ support in kitchen-service by adding @EnableRabbit

--- kitchen-service/src/main/java/ru/ershov/project/kitchenservice/KitchenServiceApplication.java
+++ kitchen-service/src/main/java/ru/ershov/project/kitchenservice/KitchenServiceApplication.java
@@ -1,8 +1,9 @@
 package ru.ershov.project.kitchenservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableRabbit
 @SpringBootApplication(scanBasePackages = {"ru.ershov.project.common", "ru.ershov.project.kitchenservice"})
 public class KitchenServiceApplication {
 
